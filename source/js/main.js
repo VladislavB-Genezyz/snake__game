533fef675c1c09a23f74344156caf2c91dc69363
VladislavB-Genezyz <vladislavb@genezyz.com>
2019-01-30 12:18:27 +0200

    Field & excel arecreated

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,29 @@
 window.onload = ()=>{
 	// alert(11);
+
+	let field = document.createElement('div')
+	document.body.appendChild(field);
+	field.classList.add('field');
+
+	for(let i = 1; i <= 100; i++){
+		let excel = document.createElement('div')
+		field.appendChild(excel);
+		excel.classList.add('excel');
+	}
+	let excel = Object.entries(document.getElementsByClassName('excel'));
+	let x = 1;
+	let y = 10;
+	// console.log(typeof(excel));
+	// console.log(excel);
+	excel.forEach((elem,index)=>{
+		if(x === 11){
+			x = 1;
+			y--;
+		}
+		elem[1].setAttribute('posX',x);
+		elem[1].setAttribute('posY',y);
+		x++;
+		
+		// console.log(index);
+	})
 }
